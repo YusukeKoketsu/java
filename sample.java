4874b169836ec6e12beaa728c9ab4a3b8cf1ad97
YusukeKoketsu <102884584+YusukeKoketsu@users.noreply.github.com>
2022-07-22 15:26:05 +0900

    first study

---
+++ sample.java
@@ -0,0 +1,39 @@
+class Main{
+  public static void main(string[] args) {
+System.out.println("Hell World");
+
+// 数値の場合はダブルクォーテーションで囲まないこと
+System.out.println(17);
+System.out.println(5+4);
+// 数値をダブルクォーテーションで囲むと文字列として出力される
+System.out.println("2+2");
+
+// 12を3で割った値を出力してください
+System.out.println(12/3);
+
+// 3に6を掛けた値を出力してください
+System.out.println(3*6);
+
+// 8を3で割った時の余りを出力してください
+System.out.println(8%3);
+
+// "こんにちは"と"世界"を連結して出力してください
+System.out.println("こんにちは"+"世界");
+
+// "38"と"19"を連結して出力してください
+System.out.println("38"+"19");
+
+// 38と19を足して出力してください
+System.out.println(38+19);
+
+// "こんにちは"と"世界"を連結して出力してください
+System.out.println("こんにちは"+"世界");
+
+// "38"と"19"を連結して出力してください
+System.out.println("38"+"19");
+
+// 38と19を足して出力してください
+System.out.println(38+19);
+
+  }
+}

387bbd49bc4cf5bc5a091ed4e30b8dc22afb9565
YusukeKoketsu <102884584+YusukeKoketsu@users.noreply.github.com>
2022-08-08 23:57:36 +0900

    java

---
+++ Lesson/src/Sample01.java
@@ -0,0 +1,26 @@
+
+//public class Sample01 {
+//	public static void main(String[] args) {
+////		処理(文)　System.out.println はJavaの標準出力用メソッド
+//		System.out.println("Hello WEBCAMP!");
+//		System.out.println("Hello Java!");
+//	}
+//}
+
+public class Sample01 {
+	public static void main(String[] args) {
+//		型 変数名;
+		int num1;
+		String str1;
+		num1 = 10;
+		str1 = "Hello World!";
+		System.out.println(num1);
+		System.out.println(str1);
+		
+		num1 = 20;
+		System.out.println(num1);
+		
+		int num2 = 30;
+		System.out.println(num2);
+	}
+}

0c611ba293fd68ab67c030edb9d901c642d511b5
YusukeKoketsu <102884584+YusukeKoketsu@users.noreply.github.com>
2022-08-09 23:59:26 +0900

    java

--- Lesson/src/Sample01.java
+++ Lesson/src/Sample01.java
@@ -7,20 +7,44 @@
 //	}
 //}
 
+//public class Sample01 {
+//	public static void main(String[] args) {
+////		型 変数名 = 代入するデータ;
+////		識別子の命名のルールに注意
+//		int num1;
+//		String str1;
+//		num1 = 10;
+//		str1 = "Hello World!";
+//		System.out.println(num1);
+//		System.out.println(str1);
+//		
+//		num1 = 20;
+//		System.out.println(num1);
+//		
+//		int num2 = 30;
+//		System.out.println(num2);
+//	}
+//}
+
+//定数の宣言 final 定数の型 定数名 = 初期値;
+//定数とは、最初に代入した値を後から変更できない変数のこと
+//public class Sample01 {
+//	public static void main(String[] args) {
+//		final int MIN_NUMBER = 10;
+//		System.out.println(MIN_NUMBER);
+//		
+////		再代入しようとするとエラー
+////		MIN_NUMBER = 20;
+//	}
+//}
+
 public class Sample01 {
 	public static void main(String[] args) {
-//		型 変数名;
-		int num1;
-		String str1;
-		num1 = 10;
-		str1 = "Hello World!";
-		System.out.println(num1);
-		System.out.println(str1);
-		
-		num1 = 20;
-		System.out.println(num1);
-		
-		int num2 = 30;
-		System.out.println(num2);
+		String name = "koketsu";
+		System.out.println(name);
+		name = "monster";
+		System.out.println(name);
+		final int num = 4;
+		System.out.println(num);
 	}
 }
---
+++ Lesson/src/progate1.java
@@ -0,0 +1,10 @@
+
+public class progate1 {
+	public static void main(String[] ages) {
+//		キャストは,強制的に型変換 (変換したいデータ型)値
+		int num1 = 7;
+		int num2 = 2;
+		System.out.println(num1/num2);
+		System.out.println((double)num1/num2);
+	}
+}

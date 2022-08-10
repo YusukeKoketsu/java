e8758b438ffb601457d7ce5239c37aa3bf5226c4
YusukeKoketsu <102884584+YusukeKoketsu@users.noreply.github.com>
2022-08-10 23:54:58 +0900

    java

--- Lesson/src/progate1.java
+++ Lesson/src/progate1.java
@@ -2,9 +2,26 @@
 public class progate1 {
 	public static void main(String[] ages) {
 //		キャストは,強制的に型変換 (変換したいデータ型)値
-		int num1 = 7;
-		int num2 = 2;
-		System.out.println(num1/num2);
-		System.out.println((double)num1/num2);
+//		int num1 = 7;
+//		int num2 = 2;
+//		System.out.println(num1/num2);
+//		System.out.println((double)num1/num2);
+		
+//		真偽値のデータ型はboolean型
+//		boolean bool = (3*9 == 27);
+//		System.out.println(bool);
+//		!(x >= 30)は「xが30以上でない（つまり30より小さい）」ときtrueになり、「xが30以上」のときfalseになる
+//		int x = 40;
+//		System.out.println(!(x>=30));
+		
+//		if文 { }の後ろにはセミコロンは不要
+		int x = 1;
+		if (x > 2) {
+			System.out.println("xは2より大きい");
+		} else if (x == 2){
+			System.out.println("xは2です");
+		} else {
+			System.out.println("xは2より小さい");
+		}
 	}
 }

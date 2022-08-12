e3983f0604b3ce6af9efc5c932fd500897ec9873
YusukeKoketsu <102884584+YusukeKoketsu@users.noreply.github.com>
2022-08-12 23:59:22 +0900

    java

--- Lesson/src/progate1.java
+++ Lesson/src/progate1.java
@@ -15,13 +15,55 @@ public class progate1 {
 //		System.out.println(!(x>=30));
 		
 //		if文 { }の後ろにはセミコロンは不要
-		int x = 1;
-		if (x > 2) {
-			System.out.println("xは2より大きい");
-		} else if (x == 2){
-			System.out.println("xは2です");
-		} else {
-			System.out.println("xは2より小さい");
-		}
+//		int x = 1;
+//		if (x > 2) {
+//			System.out.println("xは2より大きい");
+//		} else if (x == 2){
+//			System.out.println("xは2です");
+//		} else {
+//			System.out.println("xは2より小さい");
+		
+//		switch文 条件の値がcaseの値と一致するとき、処理が実行 caseの後ろはコロン(:)
+//		breakはswitch文を終了する命令
+//		int n = 3;
+//		switch (n) {
+//		 case 1:
+//		   System.out.println("大吉");
+//		   break;
+//		 case 2:
+//		   System.out.println("吉");
+//		   break;
+//		 defaultはどのcaseとも一致しなかったときに実行する処理  elseのようなもの
+//		 default:
+//		   System.out.println("凶です");
+//		   break;
+//		}
+		
+//		while文
+//		int num = 10;
+//		while (num > 0) {
+//			System.out.println(num);
+//			num--;
+//		}
+//		for文
+//		for (int i = 1; i <= 10; i++) {
+//			System.out.println(i+"回目のループ処理です");
+//		}
+//		continueはその周の処理だけをスキップして、次の周を実行する
+//		for (int j = 1; j < 10; j++) {
+//          if (j % 3 == 0){
+//		    continue;
+//		    }
+//		 System.out.println(j);
+//		}
+		
+//		配列 上書き
+		String[] names = {"纐纈", "鈴木", "山田"};
+//		System.out.println(names[0]);
+//		names[0] = "優輔";
+//		System.out.println(names[0]);
+		for (int i = 0; i < names.length; i++) {
+		   System.out.println("私の名前は"+names[i]+"です");
+		 }
 	}
 }

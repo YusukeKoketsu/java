f1b4e40e803bccd7b808c1a55d4c1f41ce2800e5
YusukeKoketsu <102884584+YusukeKoketsu@users.noreply.github.com>
2022-08-20 23:59:47 +0900

    java

--- Lesson/src/Progate3.java
+++ Lesson/src/Progate3.java
@@ -6,12 +6,31 @@ public class Progate3 {
 //	  Person.hello();
 //	  Person.printData(Person.fullName("Kate", "Jones"), 27, 1.6, 50.0);
 //	  Person.printData(Person.fullName("John", "Christopher", "Smith"), 65, 1.75, 80.0);
-      Scanner scanner = new Scanner(System.in);
-      
-      System.out.println("お名前 : ");
-      
-      String name = scanner.next();
-      
-      System.out.println("こんにちは"+name+"さん");
+	  Scanner scanner = new Scanner(System.in);
+	    
+	    System.out.println("何人分の情報を入力しますか：");
+	    int count = scanner.nextInt();
+	    
+	    for (int i = 0; i < count; i++) {
+	    
+	    System.out.println(i + 1 + "人目");
+	    
+	    System.out.println("名前：");
+	    String firstName = scanner.next();
+	    
+	    System.out.print("名字：");
+	    String lastName = scanner.next();
+	    
+	    System.out.println("年齢：");
+	    int age = scanner.nextInt();
+	    
+	    System.out.println("身長(m)：");
+	    double height = scanner.nextDouble();
+	    
+	    System.out.println("体重(kg)：");
+	    double weight = scanner.nextDouble();
+	    
+	    Person.printData(firstName, lastName, age, height, weight);
+	}
   }
 }

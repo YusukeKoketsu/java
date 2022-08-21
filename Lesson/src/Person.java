c30120d45a4bc5468d8cfc9a730fe11f0dcb0a99
YusukeKoketsu <102884584+YusukeKoketsu@users.noreply.github.com>
2022-08-21 23:55:52 +0900

    java

--- Lesson/src/Person.java
+++ Lesson/src/Person.java
@@ -1,12 +1,11 @@
 //外部ライブラリを自分のプログラムに読み込み
-import java.lang.Math;
 
 public class Person {
 //  public static void hello() {
 //	  System.out.println("Hello Java");
 //  }
-	public static void printData(String name, int age, double height, double weight) {
-	    System.out.println("私の名前は" + name + "です");
+	public static void printData(String firstName, String lastName, int age, double height, double weight) {
+	    System.out.println("私の名前は" + fullName(firstName, lastName) + "です");
 	    System.out.println("年齢は" + age + "歳です");
 	    System.out.println("身長は" + height + "mです");
 	    System.out.println("体重は" + weight + "kgです");

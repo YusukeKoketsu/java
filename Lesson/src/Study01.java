763ca2933235e4ff0b55fd770addb3cde194cd13
YusukeKoketsu <102884584+YusukeKoketsu@users.noreply.github.com>
2022-09-01 22:20:30 +0900

    java

---
+++ Lesson/src/Study01.java
@@ -0,0 +1,25 @@
+public class Study01 {
+  public static void main(String[] args) {
+/*  //	 変数の初期化
+	 int myAge;
+	 myAge = 23;
+	 System.out.println("私の歳は" + myAge + "です");
+//	 変数の上書き
+	 myAge = 24;
+	 System.out.println("今年で" + myAge + "です");
+	 
+//	 定数の宣言 final 型 定数名 = 初期値;
+//	 定数宣言後は、値を書き換えることが出来なくなる
+	 final double pi = 3.14;
+     int pie = 5;
+     double area = pie * pie * pi;
+     System.out.println("半径" + pie + "cmのパイの面積は、" + area + "です");
+*/
+     
+     int height = 3;
+     int width = 5;
+     int area = height * width;
+     System.out.println("長方形の面積は" + area + "です");
+	 
+  }
+}

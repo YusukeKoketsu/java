916d816b958ba360e3257aafff816a2785be7c88
YusukeKoketsu <102884584+YusukeKoketsu@users.noreply.github.com>
2022-09-02 20:38:16 +0900

    java

--- Lesson/src/Study01.java
+++ Lesson/src/Study01.java
@@ -1,6 +1,7 @@
 public class Study01 {
   public static void main(String[] args) {
-/*  //	 変数の初期化
+/*  
+//	 変数の初期化
 	 int myAge;
 	 myAge = 23;
 	 System.out.println("私の歳は" + myAge + "です");
@@ -14,12 +15,25 @@ public class Study01 {
      int pie = 5;
      double area = pie * pie * pi;
      System.out.println("半径" + pie + "cmのパイの面積は、" + area + "です");
-*/
+
      
      int height = 3;
      int width = 5;
      int area = height * width;
      System.out.println("長方形の面積は" + area + "です");
-	 
+     
+//	 整数リテラル  ソースコードに記述されている具体的な値のこと
+//	 0xを付けると16進数、0を付けると8進数、0bを付けると2進数として解釈される
+	 int a = 0x11;
+	 int b = 0b0011;
+	 System.out.println(a);
+	 System.out.println(b);
+*/
+//	  \ エスケープシーケンスを用いた記述
+//	  System.out.println("私の好きな記号は二重引用符(")です");
+//	  このコードでは、「)です"」が、文字列てして見なされず、エラーが起きる　その為、「\"」を用いる
+	  System.out.println("私の好きな記号は二重引用符(\")です");
+//	  金額表示
+	  System.out.println("\\1200になります");
   }
 }
